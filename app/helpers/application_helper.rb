fdeeb30f2895b6472d18a99c6012ad15ce4da469
Brunno Pleffken <brunno.pleffken@outlook.com>
2016-08-24 18:48:29 -0300

    Comparison bars... you know... those green and red...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def stats_diff(first_weapon_stat, second_weapon_stat)
+    diff_value = first_weapon_stat - second_weapon_stat
+
+    if diff_value < 0
+      second_bar = diff_value * -1;
+      html = "<div class='progress'><div class='progress-bar' style='width: #{first_weapon_stat}%'></div><div class='progress-bar upper' style='width: #{second_bar}%'></div></div>"
+    elsif diff_value > 0
+      second_bar = diff_value;
+      html = "<div class='progress'><div class='progress-bar' style='width: #{second_weapon_stat}%'></div><div class='progress-bar down' style='width: #{second_bar}%'></div></div>"
+    else
+      html = "<div class='progress'><div class='progress-bar' style='width: #{first_weapon_stat}%'></div></div>"
+    end
+
+    return html.html_safe
+  end
+
 end
