8c8eb4fbfecd0d742a48fbd74d612c9f3034223a
Brunno Pleffken <brunno.pleffken@outlook.com>
2016-08-26 13:22:43 -0300

    API key

--- config/initializers/destiny_api.rb
+++ config/initializers/destiny_api.rb
@@ -1,3 +1,3 @@
 # Insert here your Bungie API key
 # Generated from https://www.bungie.net/en/user/api
-Rails.application.config.destiny_api_key = ''
+Rails.application.config.destiny_api_key = 'e20758688ba94f4d8b18cf551c46d65b'
