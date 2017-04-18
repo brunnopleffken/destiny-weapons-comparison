6d25824e4eed602ce986a73de583cfe5df853403
Brunno Pleffken <brunno.pleffken@outlook.com>
2017-04-18 02:02:47 -0300

    Remove unused JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -53,50 +53,31 @@ $(document).ready(function() {
 
     switch ($(this).val()) {
       case 'de':
-        first = 'Lerrer Blick';
-        second = 'Y-09 Langbogen-Synthese';
         button = 'Vergleichen';
         break;
       case 'en':
-        first = '1000-Yard Stare';
-        second = 'Y-09 Longbow Synthesis';
         button = 'Compare';
         break;
       case 'es':
-        first = 'Mirada Kilométrica';
-        second = 'Síntesis de Arco Largo Y-09';
         button = 'Comparar';
         break;
       case 'fr':
-        first = 'Vue d\'aigle';
-        second = 'Synthèse d\'arc Y-09';
         button = 'Comparer';
         break;
       case 'it':
-        first = 'Sguardo Perso';
-        second = 'Y-09 Sintesi Arco Lungo';
         button = 'Confrontare';
         break;
       case 'jp':
-        first = '1000mの凝視';
-        second = 'Y-09 ロングボウ合成';
         button = '比べる';
         break;
       case 'pt-br':
-        first = 'Encarada de 1000 Jardas';
-        second = 'Síntese do Arco Longo Y-09';
         button = 'Comparar';
         break;
       default:
-        first = '1000-Yard Stare';
-        second = 'Y-09 Longbow Synthesis';
         button = 'Compare';
     }
 
-    $('#primary-weapon').attr('placeholder', first);
-    $('#secondary-weapon').attr('placeholder', second);
     $('#compare-button').html(button);
-
   });
 
 
